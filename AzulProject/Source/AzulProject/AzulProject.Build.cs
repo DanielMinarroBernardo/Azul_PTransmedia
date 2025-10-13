311ea5dce95163a8d14e6facc069a4d68e519d12
Carla De La Loma <144267369+carladelaloma@users.noreply.github.com>
2025-10-13 15:31:54 +0200

    Tecla E
    He hecho la interacción de la tecla E con interfaces.

--- AzulProject/Source/AzulProject/AzulProject.Build.cs
+++ AzulProject/Source/AzulProject/AzulProject.Build.cs
@@ -8,7 +8,7 @@ public class AzulProject : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Niagara" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
--- AzulProject/Source/AzulProject/Private/Hilo.cpp
+++ AzulProject/Source/AzulProject/Private/Hilo.cpp
@@ -9,6 +9,18 @@ AHilo::AHilo()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// --- Trigger de colisión (Root) ---
+	//TriggerBox = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerBox"));
+	//SetRootComponent(TriggerBox);
+	//TriggerBox->InitBoxExtent(FVector(100.f, 100.f, 100.f));
+	//TriggerBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	//TriggerBox->SetCollisionObjectType(ECC_WorldDynamic);
+	//TriggerBox->SetCollisionResponseToAllChannels(ECR_Ignore);
+	//TriggerBox->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	//TriggerBox->SetGenerateOverlapEvents(true);
+
+	// Bind de evento de overlap
+	//TriggerBox->OnComponentBeginOverlap.AddDynamic(this, &AHilo::OnBoxBeginOverlap);
 }
 
 // Called when the game starts or when spawned
@@ -33,7 +45,7 @@ void AHilo::Tick(float DeltaTime)
 
 }
 
-void AHilo::SetSpline()
+void AHilo::SetShapeSpline()
 {
 	Spline->ClearSplinePoints(false);
 
@@ -45,4 +57,24 @@ void AHilo::SetSpline()
 	Spline->UpdateSpline();
 
 }
+
+void AHilo::SetLengthHilo()
+{
+	NiagaraHilo = Cast<UNiagaraComponent>(GetDefaultSubobjectByName(TEXT("HiloNiagara")));
+
+	const float Length = Spline->GetSplineLength();
+
+	NiagaraHilo->SetFloatParameter(FName(TEXT("SpawnRate")), Length);
+	NiagaraHilo->SetFloatParameter(FName(TEXT("User.SpawnRate")), Length);
+}
+
+//void AHilo::OnBoxBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+//{
+//	// Evitar auto-disparo y filtrar a Pawns (jugador/NPCs)
+//	if (OtherActor && OtherActor != this && OtherActor->IsA(APawn::StaticClass()))
+//	{
+//		UE_LOG(LogTemp, Log, TEXT("Overlap con %s -> SetSpline()"), *OtherActor->GetName());
+//		SetSpline();
+//	}
+//}
 
